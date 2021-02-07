1e9b0e7bbd96a4394f5c358ce613c7a3be008354
Adw41t <adwait.a.samant@gmail.com>
2021-02-07 15:58:35 +0530

    Changed package name

---
+++ app/src/main/java/com/example/android/notesapp/tasks/TasksAdapterNew.kt
@@ -0,0 +1,27 @@
+package com.example.android.notesapp.tasks
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.notesapp.data.Task
+
+class TasksAdapterNew(private var list: List<Task>,val context: Context): RecyclerView.Adapter<TasksViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
+//        val view = LayoutInflater.from(context).inflate(R.layout.task_item,parent,false)
+//        return TasksViewHolder(view)
+        return TasksViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
+        return holder.bind(context, list.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    fun updateList(list: List<Task>){
+        this.list = list
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/example/android/notesapp/tasks/TasksViewHolder.kt
@@ -0,0 +1,32 @@
+package com.example.android.notesapp.tasks
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View.GONE
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.notesapp.R
+import com.example.android.notesapp.data.Task
+import com.example.android.notesapp.databinding.TaskItemBinding
+
+class TasksViewHolder private constructor(val binding: TaskItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    companion object {
+        fun from(parent: ViewGroup): TasksViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+
+            return TasksViewHolder(binding)
+        }
+    }
+    val titleText: TextView = itemView.findViewById(R.id.title_text)
+    val description: TextView = itemView.findViewById(R.id.description_text)
+    fun bind(context: Context,task: Task){
+        titleText.setText(task.title)
+        if(task.description.isNotBlank())
+            description.setText(task.description)
+        else
+            description.visibility=GONE
+    }
+}
